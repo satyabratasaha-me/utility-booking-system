f35f9db45df51fdcc769078db7c24a43d3b16b8f
Coder Tester <codewithme@outlook.com>
2017-12-09 04:54:51 +0530

    heroku test

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ class App extends React.Component {
             <tr>
                 <td/>
                 <td>
-                    <Button name='login' value='Login'></Button>
+                    <Button name='login' value='Sbmit'></Button>
                 </td>
              </tr>  
          </div>
